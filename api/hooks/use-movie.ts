79768779a1c0abd613472f4b4381c63c4bee8128
nmphuong57007 <nmphuong57007@gmail.com>
2025-11-09 10:20:32 +0700

    feat:api movies

---
+++ api/hooks/use-movie.ts
@@ -0,0 +1,13 @@
+import { useQuery, queryOptions } from "@tanstack/react-query";
+
+import { hasToken } from "@/helpers/has-token";
+import { getMovies } from "../services/movie-api";
+
+export const useMovies = (per_page?: number, page?: number) => {
+  return useQuery({
+    queryKey: ["getMovies", per_page, page],
+    queryFn: () => getMovies(per_page, page),
+    enabled: hasToken(),
+    ...queryOptions,
+  });
+};
